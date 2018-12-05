823d7f93e972f23debdb15323566a8cf82da4bd3
김성훈 <seongh7800@douzone.com>
2018-12-05 09:17:32 +0900

    답변목록 추가

--- routes/questions.js
+++ routes/questions.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+//
+const groups = require('./data/groups.json')
+const questions = require('./data/questions.json')
+const answers = require('./data/answers.json')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send(200, { title: 'Express' });
+});
+
+/**
+ * 
+ */
+router.get('/:id/answers', function(req, res, next) {
+  const id = Number(req.params.id);    
+  const data = answers.filter(answer => answer.question_idx === id)
+
+  res.send(200, { 
+      status : 200,
+      message: "success",
+      data
+  });
 });
 
 module.exports = router;
